fbb0ea3f01539aaa7aae90c9b5cdced8a52d9143
NicoDiazFk <dbzsuper999@gmail.com>
2024-05-11 22:02:38 -0500

    MOVIMIENTO + CAMARA

---
+++ proyecoto_Mario64/Assets/script/MovimientoQ.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovimientoQ : MonoBehaviour
+{
+    public float moveSpeed = 10f;
+    public float rotationSpeed = 100f;
+    private Rigidbody rb;
+
+    // public Animator anima;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float horinzontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        Vector3 movement = transform.forward * moveSpeed * verticalInput;
+
+        Quaternion rotation = Quaternion.Euler(0f, horinzontalInput * rotationSpeed
+            * Time.fixedDeltaTime, 0f);
+
+        rb.MovePosition(rb.position + movement * Time.fixedDeltaTime);
+        rb.MoveRotation(rb.rotation * rotation);
+        /*
+        if (verticalInput > 0 || verticalInput < 0)
+        {
+            anima.SetFloat("Walk", Mathf.Abs(verticalInput));
+        }
+        else
+        {
+            anima.SetFloat("Walk", 0);
+        }
+        */
+
+
+    }
+}
